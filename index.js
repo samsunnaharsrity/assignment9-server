2c564380c53a8f9b412dc24b9bf2012008b8eadb
Samsunnahar Srity <samsunnaharsrity@gmail.com>
2026-05-23 10:58:39 +0600

    fix vercel

--- index.js
+++ index.js
@@ -504,6 +504,7 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+module.exports = app;
