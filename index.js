a3285ad91dfb3cdb28ce17f23cdea586fa6e268f
Samsunnahar Srity <samsunnaharsrity@gmail.com>
2026-05-24 09:44:17 +0600

    fixed vercel

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ const verifyToken = async (req, res, next) => {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("StudyNook");
 
